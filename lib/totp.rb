4a65f7ca2bdd60bfa82c2ed527e0c03bf3df434d
susurri <susurrus.silent@gmail.com>
2016-10-17 01:06:22 +0900

    style: rubocop

--- lib/totp.rb
+++ lib/totp.rb
@@ -1,7 +1,8 @@
-require "totp/version"
-require "totp/secrets"
-require "totp/cli"
+require 'totp/version'
+require 'totp/secrets'
+require 'totp/cli'
 
+# time based one time password
 module Totp
   # Your code goes here...
 end
